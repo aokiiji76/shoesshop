4cd1c163e0c2fd3cd6c3939289d05dd0d8c8013e
JulienLh <aokiiji76@gmail.com>
2022-02-25 16:06:39 +0100

    nav et accueil dyna

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\BrandRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main')]
-    public function accueil(CategoryRepository $categoryRepository): Response
-    {
-       $categories =  $categoryRepository->HomeOrderCategory();
-
-        //dd($categories);
+    #[Route('/', name: 'accueil')]
+    public function accueil(
+        CategoryRepository $categoryRepository,
+        TypeRepository $typeRepository,
+        BrandRepository $brandRepository
+        ): Response
+    { 
+       //dd($categories);
         return $this->render('pages/accueil.html.twig', [
-            'categories' => $categories,
+            'categories' => $categoryRepository->HomeOrderCategory(),
+            'types'=>$typeRepository->findAll(),
+            'brands'=>$brandRepository->findAll(),
         ]);
     }
 }
