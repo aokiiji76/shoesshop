69676df3d9e2bf64253aca3403f4be142c3d3866
JulienLh <aokiiji76@gmail.com>
2022-03-10 18:36:26 +0100

    test fixture a revoir

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
 use Faker\Factory;
 
 use App\Entity\Product;
+use App\Repository\BrandRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\TypeRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -13,34 +15,47 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 class AppFixtures extends Fixture
 {
     private $faker;
-    private $categories;
+    private $category;
+    private $type;
+    private $brand;
+    private $em;
+
+    public function __construct(
+        CategoryRepository $categoryRepository,
+        TypeRepository $typeRepository,
+        BrandRepository $brandRepository)
+    {
+        $this->category = $categoryRepository->findAll();
+        $this->type = $typeRepository->findAll();
+        $this->brand = $brandRepository->findAll();
+        
+    }
 
     public function load(ObjectManager $manager): void
     {
         $this->faker = Factory::create('fr_FR');
-        $this->addProduct($em);
+        $this->addProduct($manager);
         
         
         $manager->flush();
     }
 
-    private function addProduct(EntityManager $em,Category $category)
+    private function addProduct($manager)
     {
+
         for ($i = 1; $i <= 10; $i++) {
             $product = new Product();
             $product->setName($this->faker->word(5));
             $product->setDescription($this->faker->paragraph());
             $product->setPicture($this->faker->imageUrl('https://via.placeholder.com/300')); 
             $product->setPrice($this->faker->numberBetween(40, 150)); 
-            $product->setRate($this->faker->numberBetween(0, 5));
+            $product->setRate($this->faker->numberBetween(0, 6));
             $product->setStatus('1');
-            $product->setCategory($category[rand(0, count($category))]);
-            $product->setType();
-            $product->setBrand();
-
-            $user->setFirstName($firstname);
-            $em->persist($user);
-            $this->addPosts($user);
+            $product->setCategory($this->category[rand(1,count($this->category))]);
+            $product->setType($this->type[rand(1,count($this->type))]);
+            $product->setBrand($this->brand[rand(1,count($this->brand))]);
+            $manager->persist($product);
+           
         }
     }
 
